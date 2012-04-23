92dc629a4cf3153f9b4e06c0a3003eac17305801
Tobias Tangemann <tobiastangemann@gmail.com>
2012-04-23 14:33:04 +0200

    * Previous and next song implemented * Previous song not working 100%

--- Google Music Helper/AppDelegate.h
+++ Google Music Helper/AppDelegate.h
@@ -16,10 +16,14 @@
   
   NSAppleScript* playPauseScript;
   NSAppleScript* searchScript;  
+  NSAppleScript* nextSongScript;
+  NSAppleScript* previousSongScript;
 }
 
 - (IBAction)playPause:(id)sender;
 - (IBAction)searchSong:(id)sender;
+- (IBAction)nextSong:(id)sender;
+- (IBAction)previousSong:(id)sender;
 
 @property (assign) IBOutlet NSWindow *window;
 
--- Google Music Helper/AppDelegate.m
+++ Google Music Helper/AppDelegate.m
@@ -32,10 +32,18 @@
   NSString* path_search = [[NSBundle mainBundle] pathForResource:@"search" ofType:@"scpt"];
   NSURL* url_search = [NSURL fileURLWithPath:path_search];
   
+  NSString* path_nextSong = [[NSBundle mainBundle] pathForResource:@"nextSong" ofType:@"scpt"];
+  NSURL* url_nextSong = [NSURL fileURLWithPath:path_nextSong];
+  
+  NSString* path_previousSong = [[NSBundle mainBundle] pathForResource:@"prevSong" ofType:@"scpt"];
+  NSURL* url_previousSong = [NSURL fileURLWithPath:path_previousSong];
+  
   NSDictionary* errors = [NSDictionary dictionary];
   
   playPauseScript = [[NSAppleScript alloc] initWithContentsOfURL:url_playPause error:&errors];
   searchScript = [[NSAppleScript alloc] initWithContentsOfURL:url_search error:&errors];
+  nextSongScript = [[NSAppleScript alloc] initWithContentsOfURL:url_nextSong error:&errors];
+  previousSongScript = [[NSAppleScript alloc] initWithContentsOfURL:url_previousSong error:&errors];
 }
 
 - (IBAction)playPause:(id)sender {
@@ -45,11 +53,25 @@
   [playPauseScript executeAndReturnError:nil];  
 }
 
-- (IBAction)searchSong:(id)sender {  
+- (IBAction)searchSong:(id)sender {
   [NSApp activateIgnoringOtherApps:YES];
   NSLog(@"Search music");
   
   [searchScript executeAndReturnError:nil];
+}
+
+- (IBAction)nextSong:(id)sender {  
+  [NSApp activateIgnoringOtherApps:YES];
+  NSLog(@"Search music");
+  
+  [nextSongScript executeAndReturnError:nil];
+}
+
+- (IBAction)previousSong:(id)sender {
+  [NSApp activateIgnoringOtherApps:YES];
+  NSLog(@"Search music");
+  
+  [previousSongScript executeAndReturnError:nil];
 }
 
 @end
